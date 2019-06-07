5045c51d12a43148a722b7861610aae1df191719
Luca Pinna <>
2019-06-07 23:36:35 +0200

    fix null resource id

--- azurerm_cosmosdb_container/main.tf
+++ azurerm_cosmosdb_container/main.tf
@@ -15,71 +15,53 @@ resource "azurerm_cosmosdb_sql_database" "cosmosdb_sql_database" {
   account_name        = "${data.azurerm_cosmosdb_account.cosmosdb_account.name}"
 }
 
-
 module "azurerm_cosmosdb_sql_container" {
-  source              = "git@github.com:teamdigitale/terraform-azurerm-resource.git"
-  api_version         = "2016-03-31"
-  type                = "Microsoft.DocumentDB/databaseAccounts/apis/databases/containers"
-  name                = "${data.azurerm_cosmosdb_account.cosmosdb_account.name}/sql/${local.azurerm_cosmosdb_documentdb_name}/testcontainer"
-  resource_group_name = "${data.azurerm_resource_group.rg.name}"
-  location            = "${var.location}"
+  #   source              = "git@github.com:teamdigitale/terraform-azurerm-resource.git"
+#   deployment_mode        = "Complete"
+  source                 = "./terraform-azurerm-resource"
+  api_version            = "2016-03-31"
+  type                   = "Microsoft.DocumentDB/databaseAccounts/apis/databases/containers"
+  name                   = "${data.azurerm_cosmosdb_account.cosmosdb_account.name}/sql/${local.azurerm_cosmosdb_documentdb_name}/messages"
+  resource_group_name    = "${data.azurerm_resource_group.rg.name}"
+  location               = "${var.location}"
+  random_deployment_name = true
+
   # tags                = "${var.tags}"
 
-properties {
-    resource = <<EOF
-    {
-    	"id": "testcontainer",
-			"partitionKey": {
-						"paths": [
-						"/MyPartitionKey1"
-						],
-						"kind": "Hash"
-			},
-    	"indexingPolicy": {
-    		"indexingMode": "consistent",
-    		"automatic": true,
-    		"includedPaths": [{
-    			"path": "/*",
-    			"indexes": [{
-    					"kind": "Range",
-    					"dataType": "Number",
-    					"precision": -1
-    				},
-    				{
-    					"kind": "Range",
-    					"dataType": "String",
-    					"precision": -1
-    				},
-    				{
-    					"kind": "Spatial",
-    					"dataType": "Point"
-    				}
-    			]
-    		}],
-    		"excludedPaths": []
-    	},
-    	"partitionKey": {
-    		"paths": [
-    			"string"
-    		],
-    		"kind": "string"
-    	},
-    	"defaultTtl": "integer",
-    	"uniqueKeyPolicy": {
-    		"uniqueKeys": [{
-    			"paths": [
-    				"string"
-    			]
-    		}]
-    	},
-    	"conflictResolutionPolicy": {
-    		"mode": "string",
-    		"conflictResolutionPath": "string",
-    		"conflictResolutionProcedure": "string"
-    	}
+  properties {
+    resource {
+      id = "messages"
+
+      partitionKey = {
+        paths = ["/fiscalCode"]
+        kind  = "Hash"
+      }
+
+      indexingPolicy = {
+        indexingMode = "lazy"
+automatic = true
+        includedPaths = [
+          {
+            path = "/*"
+
+            indexes = [
+              {
+                kind      = "Range"
+                dataType  = "number"
+                precision = -1
+              },
+                            {
+                kind      = "Hash"
+                dataType  = "String"
+                precision = 3
+              }
+            ]
+          }
+        ]
+        excludedPaths = []
+      }
     }
-    EOF
-    options = {}
+    # options = {}
   }
 
   # sku {
@@ -87,3 +69,48 @@ properties {
   #   capacity = "${var.sku_capacity}"
   # }
 }
+
+# data "template_file" "init" {
+# #   vars = {
+# #     consul_address = "${aws_instance.consul.private_ip}"
+# #   }
+#   template = <<EOF
+# {
+#                     "id": "testcontainer",
+#                     "partitionKey": {
+#                         "paths": [
+#                             "/MyPartitionKey1"
+#                         ],
+#                         "kind": "Hash"
+#                     },
+#                     "indexingPolicy": {
+#                         "indexingMode": "consistent",
+#                         "includedPaths": [
+#                             {
+#                                 "path": "/*",
+#                                 "indexes": [
+#                                     {
+#                                         "kind": "Range",
+#                                         "dataType": "number",
+#                                         "precision": -1
+#                                     },
+#                                     {
+#                                         "kind": "Range",
+#                                         "dataType": "string",
+#                                         "precision": -1
+#                                     }
+#                                 ]
+#                             }
+#                         ],
+#                         "excludedPaths": [
+#                             {
+#                                 "path": "/MyPathToNotIndex/*"
+#                             }
+#                         ]
+#                     }
+#                 }
+# EOF
+
+
+# }
+
