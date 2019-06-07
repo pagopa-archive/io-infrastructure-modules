95d2fcb1c5c38da0f0a9cac5280ebe2098089815
Roberto Chessa <r.chessa@beetobit.com>
2019-06-07 15:00:23 +0200

    [#166538291] add module to create storage queue

---
+++ azurerm_storage_queue/main.tf
@@ -0,0 +1,13 @@
+data "azurerm_resource_group" "rg" {
+  name = "${local.azurerm_resource_group_name}"
+}
+
+data "azurerm_storage_account" "sa" {
+    name                 = "${local.azurerm_storage_account_name}"
+    resource_group_name  = "${data.azurerm_resource_group.rg.name}"
+}
+resource "azurerm_storage_queue" "queue" {
+  name                 = "${var.storage_queue_name}"
+  resource_group_name  = "${data.azurerm_resource_group.rg.name}"
+  storage_account_name = "${data.azurerm_storage_account.sa.name}"
+}
