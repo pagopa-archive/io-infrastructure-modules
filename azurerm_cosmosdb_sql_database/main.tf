b3f9fea4cf1d0632cfafb35bdbac086e53972bf2
Luca Prete <lucaprete@teamdigitale.governo.it>
2019-06-10 13:29:33 +0200

    Update main.tf

--- azurerm_cosmosdb_sql_database/main.tf
+++ azurerm_cosmosdb_sql_database/main.tf
@@ -1,4 +1,4 @@
-# New infrastructure
+# Existing infrastructure
 
 data "azurerm_resource_group" "rg" {
   name = "${local.azurerm_resource_group_name}"
@@ -8,6 +8,8 @@ data "azurerm_cosmosdb_account" "cosmosdb_account" {
   name                = "${local.azurerm_cosmosdb_account_name}"
   resource_group_name = "${data.azurerm_resource_group.rg.name}"
 }
+
+# New infrastructure
 
 resource "azurerm_cosmosdb_sql_database" "cosmosdb_sql_database" {
   name                = "${local.azurerm_cosmosdb_documentdb_name}"
