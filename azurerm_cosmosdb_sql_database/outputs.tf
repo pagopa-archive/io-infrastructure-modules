df69fdf3eb35fef3c935dafb9d85920a7f8973eb
Luca Pinna <>
2019-06-10 12:45:57 +0200

    revieed

--- azurerm_cosmosdb_sql_database/outputs.tf
+++ azurerm_cosmosdb_sql_database/outputs.tf
@@ -1,1 +1,9 @@
+output "azurerm_cosmosdb_documentdb_name" {
+  description = "Cosmos DB database name."
+  value       = "${local.azurerm_cosmosdb_documentdb_name}"
+}
 
+output "azurerm_cosmosdb_documentdb_id" {
+  description = "Cosmos DB database id."
+  value       = "${azurerm_cosmosdb_sql_database.cosmosdb_sql_database.id}"
+}
--- azurerm_cosmosdb_sql_database/variables.tf
+++ azurerm_cosmosdb_sql_database/variables.tf
@@ -12,11 +12,6 @@ variable "resource_name_prefix" {
   description = "The prefix used to name all resources created."
 }
 
-variable "tags" {
-  default     = {}
-  description = "Tags that are associated with the resource."
-}
-
 # CosmosDB specific variables
 
 variable "documentdb_name" {
@@ -29,11 +24,10 @@ variable "cosmosdb_account_name" {
 }
 
 locals {
-  # Define resource names based on the following convention:
-  # {azurerm_resource_name_prefix}-RESOURCE_TYPE-{environment}
-  azurerm_resource_group_name = "${var.resource_name_prefix}-${var.environment}-rg"
+  # Define resource names based on the following convention:  # {azurerm_resource_name_prefix}-RESOURCE_TYPE-{environment}
 
-  azurerm_cosmosdb_account_name = "${var.resource_name_prefix}-${var.environment}-cosmosdb-${var.cosmosdb_account_name}"
-
+  azurerm_resource_group_name      = "${var.resource_name_prefix}-${var.environment}-rg"
+  azurerm_cosmosdb_account_name    = "${var.resource_name_prefix}-${var.environment}-cosmosdb-${var.cosmosdb_account_name}"
   azurerm_cosmosdb_documentdb_name = "${var.resource_name_prefix}-documentdb-${var.environment}"
 }
+
