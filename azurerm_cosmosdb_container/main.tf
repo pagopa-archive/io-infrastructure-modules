9e5527206c98a2812bb7230cf8f9cd907b940d97
Luca Pinna <>
2019-06-10 11:55:22 +0200

    clean up

--- azurerm_cosmosdb_container/main.tf
+++ azurerm_cosmosdb_container/main.tf
@@ -9,108 +9,27 @@ data "azurerm_cosmosdb_account" "cosmosdb_account" {
   resource_group_name = "${data.azurerm_resource_group.rg.name}"
 }
 
-resource "azurerm_cosmosdb_sql_database" "cosmosdb_sql_database" {
-  name                = "${local.azurerm_cosmosdb_documentdb_name}"
-  resource_group_name = "${data.azurerm_resource_group.rg.name}"
-  account_name        = "${data.azurerm_cosmosdb_account.cosmosdb_account.name}"
-}
-
 module "azurerm_cosmosdb_sql_container" {
-  #   source              = "git@github.com:teamdigitale/terraform-azurerm-resource.git"
-#   deployment_mode        = "Complete"
-  source                 = "./terraform-azurerm-resource"
+  source                 = "git@github.com:teamdigitale/terraform-azurerm-resource.git"
   api_version            = "2016-03-31"
   type                   = "Microsoft.DocumentDB/databaseAccounts/apis/databases/containers"
-  name                   = "${data.azurerm_cosmosdb_account.cosmosdb_account.name}/sql/${local.azurerm_cosmosdb_documentdb_name}/messages"
+  enable_output          = false
+  name                   = "${data.azurerm_cosmosdb_account.cosmosdb_account.name}/sql/${length(var.documentdb_name) >0 ? var.documentdb_name : local.azurerm_cosmosdb_documentdb_name}/${var.container_name}"
   resource_group_name    = "${data.azurerm_resource_group.rg.name}"
   location               = "${var.location}"
   random_deployment_name = true
-
-  # tags                = "${var.tags}"
+  tags                   = "${var.tags}"
 
   properties {
     resource {
-      id = "messages"
+      id = "${var.container_name}"
 
       partitionKey = {
-        paths = ["/fiscalCode"]
+        paths = ["${var.partitionKey_paths}"]
         kind  = "Hash"
       }
-
-      indexingPolicy = {
-        indexingMode = "lazy"
-automatic = true
-        includedPaths = [
-          {
-            path = "/*"
-
-            indexes = [
-              {
-                kind      = "Range"
-                dataType  = "number"
-                precision = -1
-              },
-                            {
-                kind      = "Hash"
-                dataType  = "String"
-                precision = 3
-              }
-            ]
-          }
-        ]
-        excludedPaths = []
-      }
     }
-    # options = {}
+
+    options = {}
   }
-
-  # sku {
-  #   name     = "${var.sku_name}"
-  #   capacity = "${var.sku_capacity}"
-  # }
 }
-
-# data "template_file" "init" {
-# #   vars = {
-# #     consul_address = "${aws_instance.consul.private_ip}"
-# #   }
-#   template = <<EOF
-# {
-#                     "id": "testcontainer",
-#                     "partitionKey": {
-#                         "paths": [
-#                             "/MyPartitionKey1"
-#                         ],
-#                         "kind": "Hash"
-#                     },
-#                     "indexingPolicy": {
-#                         "indexingMode": "consistent",
-#                         "includedPaths": [
-#                             {
-#                                 "path": "/*",
-#                                 "indexes": [
-#                                     {
-#                                         "kind": "Range",
-#                                         "dataType": "number",
-#                                         "precision": -1
-#                                     },
-#                                     {
-#                                         "kind": "Range",
-#                                         "dataType": "string",
-#                                         "precision": -1
-#                                     }
-#                                 ]
-#                             }
-#                         ],
-#                         "excludedPaths": [
-#                             {
-#                                 "path": "/MyPathToNotIndex/*"
-#                             }
-#                         ]
-#                     }
-#                 }
-# EOF
-
-
-# }
-
--- azurerm_cosmosdb_container/variables.tf
+++ azurerm_cosmosdb_container/variables.tf
@@ -12,18 +12,35 @@ variable "resource_name_prefix" {
   description = "The prefix used to name all resources created."
 }
 
+variable "tags" {
+  default     = {}
+  description = "Tags that are associated with the resource."
+}
+
 # CosmosDB specific variables
 
+variable "container_name" {
+  description = "Cosmos DB container name."
+}
+
+variable "documentdb_name" {
+  description = "Cosmos DB database name."
+  default     = ""
+}
+
+variable "partitionKey_paths" {
+  description = "List of paths using which data within the container can be partitioned"
+  default     = []
+}
+
 variable "cosmosdb_account_name" {
-  description = "The suffix used for the CosmosDB name."
+  description = "Cosmos DB account name."
 }
 
 locals {
-  # Define resource names based on the following convention:
-  # {azurerm_resource_name_prefix}-RESOURCE_TYPE-{environment}
-  azurerm_resource_group_name = "${var.resource_name_prefix}-${var.environment}-rg"
+  # Define resource names based on the following convention:  {azurerm_resource_name_prefix}-RESOURCE_TYPE-{environment}
 
-  azurerm_cosmosdb_account_name = "${var.resource_name_prefix}-${var.environment}-cosmosdb-${var.cosmosdb_account_name}"
-
+  azurerm_resource_group_name      = "${var.resource_name_prefix}-${var.environment}-rg"
+  azurerm_cosmosdb_account_name    = "${var.resource_name_prefix}-${var.environment}-cosmosdb-${var.cosmosdb_account_name}"
   azurerm_cosmosdb_documentdb_name = "${var.resource_name_prefix}-documentdb-${var.environment}"
 }
