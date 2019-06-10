d4ec2eb55ae16b2e55e2e2d151a46f04dd30f40c
Luca <45258018+l-pinna@users.noreply.github.com>
2019-06-10 13:34:47 +0200

    166326149 add cosmos db container

---
+++ azurerm_cosmosdb_container/main.tf
@@ -0,0 +1,40 @@
+# Existing infrastructure
+
+data "azurerm_resource_group" "rg" {
+  name = "${local.azurerm_resource_group_name}"
+}
+
+data "azurerm_cosmosdb_account" "cosmosdb_account" {
+  name                = "${local.azurerm_cosmosdb_account_name}"
+  resource_group_name = "${data.azurerm_resource_group.rg.name}"
+}
+
+# New infrastructure
+
+module "azurerm_cosmosdb_sql_container" {
+  source                 = "git@github.com:teamdigitale/terraform-azurerm-resource.git"
+  api_version            = "2016-03-31"
+  type                   = "Microsoft.DocumentDB/databaseAccounts/apis/databases/containers"
+  enable_output          = false
+  name                   = "${data.azurerm_cosmosdb_account.cosmosdb_account.name}/sql/${length(var.documentdb_name) >0 ? var.documentdb_name : local.azurerm_cosmosdb_documentdb_name}/${var.container_name}"
+  resource_group_name    = "${data.azurerm_resource_group.rg.name}"
+  location               = "${var.location}"
+  random_deployment_name = true
+
+  tags = {
+    environment = "${var.environment}"
+  }
+
+  properties {
+    resource {
+      id = "${var.container_name}"
+
+      partitionKey = {
+        paths = ["${var.partitionKey_paths}"]
+        kind  = "Hash"
+      }
+    }
+
+    options = {}
+  }
+}
---
+++ azurerm_cosmosdb_container/variables.tf
@@ -0,0 +1,41 @@
+# Generic variables
+
+variable "environment" {
+  description = "The nick name identifying the type of environment (i.e. test, staging, production)"
+}
+
+variable "location" {
+  description = "The data center location where all resources will be put into."
+}
+
+variable "resource_name_prefix" {
+  description = "The prefix used to name all resources created."
+}
+
+# CosmosDB specific variables
+
+variable "container_name" {
+  description = "Cosmos DB container name."
+}
+
+variable "documentdb_name" {
+  description = "Cosmos DB database name."
+  default     = ""
+}
+
+variable "partitionKey_paths" {
+  description = "List of paths using which data within the container can be partitioned"
+  default     = []
+}
+
+variable "cosmosdb_account_name" {
+  description = "Cosmos DB account name."
+}
+
+locals {
+  # Define resource names based on the following convention:  {azurerm_resource_name_prefix}-RESOURCE_TYPE-{environment}
+
+  azurerm_resource_group_name      = "${var.resource_name_prefix}-${var.environment}-rg"
+  azurerm_cosmosdb_account_name    = "${var.resource_name_prefix}-${var.environment}-cosmosdb-${var.cosmosdb_account_name}"
+  azurerm_cosmosdb_documentdb_name = "${var.resource_name_prefix}-documentdb-${var.environment}"
+}
