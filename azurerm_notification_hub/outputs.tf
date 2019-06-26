b917407064ecc587c8cbb7740f8660bd6a502750
Luca Prete <preteluca@gmail.com>
2019-06-26 12:01:33 +0200

    [#166896901] Add Azure notification hub module

---
+++ azurerm_notification_hub/outputs.tf
@@ -0,0 +1,9 @@
+output "azurerm_notification_hub_namespace_name" {
+  description = "The name of the notification hub namespace."
+  value       = "${local.azurerm_notification_hub_namespace_name}"
+}
+
+output "azurerm_notification_hub_name" {
+  description = "The name of the notification hub."
+  value       = "${local.azurerm_notification_hub_name}"
+}
---
+++ azurerm_notification_hub/variables.tf
@@ -0,0 +1,30 @@
+# Generic variables
+variable "environment" {
+  description = "The nick name identifying the type of environment (i.e. test, staging, production)."
+}
+
+variable "location" {
+  description = "The data center location where all resources will be put into."
+}
+
+variable "resource_name_prefix" {
+  description = "The prefix used to name all resources created."
+}
+
+# Notification Hub related variables
+
+variable "notification_hub_name" {
+  description = "The name of the notification hub."
+}
+
+variable "azurerm_notification_hub_namespace_sku_name" {
+  description = "The SKU name of the notification hub namespace."
+}
+
+locals {
+  # Define resource names based on the following convention:
+  # {azurerm_resource_name_prefix}-RESOURCE_TYPE-{environment}
+  azurerm_resource_group_name             = "${var.resource_name_prefix}-${var.environment}-rg"
+  azurerm_notification_hub_namespace_name = "${var.resource_name_prefix}-${var.environment}-nhns-${var.notification_hub_name}"
+  azurerm_notification_hub_name           = "${var.resource_name_prefix}-${var.environment}-nh-${var.notification_hub_name}"
+}
