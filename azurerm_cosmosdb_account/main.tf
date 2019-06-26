c027b17b6c17997155f43aad5b5eea8f34247fef
Roberto Chessa <r.chessa@beetobit.com>
2019-06-26 12:39:48 +0200

    [#15842229] enable cosmosdb vnet access

--- azurerm_cosmosdb_account/main.tf
+++ azurerm_cosmosdb_account/main.tf
@@ -4,6 +4,12 @@ data "azurerm_resource_group" "rg" {
   name = "${local.azurerm_resource_group_name}"
 }
 
+data "azurerm_subnet" "functions" {
+  name                 = "${local.azurerm_subnet_name}"
+  resource_group_name  = "${data.azurerm_resource_group.rg.name}"
+  virtual_network_name = "${local.azurerm_virtual_network_name}"
+}
+
 # New infrastructure
 
 resource "azurerm_cosmosdb_account" "cosmosdb_account" {
@@ -14,11 +20,14 @@ resource "azurerm_cosmosdb_account" "cosmosdb_account" {
   kind                      = "${var.azurerm_cosmosdb_account_kind}"
 
   enable_automatic_failover = true
-
   consistency_policy        = ["${var.azurerm_cosmosdb_account_consistency_policy}"]
 
   # TODO: create arbitrary number of geo_locations with terraform 0.12
-
-  geo_location              = ["${var.azurerm_cosmosdb_account_geo_location_master}"]
-  geo_location              = ["${var.azurerm_cosmosdb_account_geo_location_slave}"]
+  geo_location                      = ["${var.azurerm_cosmosdb_account_geo_location_master}"]
+  geo_location                      = ["${var.azurerm_cosmosdb_account_geo_location_slave}"]
+  is_virtual_network_filter_enabled = "${var.azurerm_cosmosdb_account_virtual_network_filter_enabled}"
+  # TODO: create arbitrary number of virtual_network_rule with terraform 0.12
+  virtual_network_rule = {
+    id = "${data.azurerm_subnet.functions.id}"
+  }
 }
