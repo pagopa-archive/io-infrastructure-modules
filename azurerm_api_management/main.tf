4208561903ad1c0c31d6b54920cab31b7340dff5
Luca Pinna <>
2019-07-10 17:23:48 +0200

    add groups

--- azurerm_api_management/main.tf
+++ azurerm_api_management/main.tf
@@ -21,18 +21,6 @@ data "azurerm_subnet" "apim_subnet" {
   resource_group_name  = "${data.azurerm_resource_group.rg.name}"
 }
 
-# Client ID of an application used in the API management portal authentication flow
-data "azurerm_key_vault_secret" "dev_portal_client_id" {
-  name         = "dev-portal-client-id-${var.environment}"
-  key_vault_id = "${var.key_vault_id}"
-}
-
-# Client secret of the application used in the API management portal authentication flow
-data "azurerm_key_vault_secret" "dev_portal_client_secret" {
-  name         = "dev-portal-client-secret-${var.environment}"
-  key_vault_id = "${var.key_vault_id}"
-}
-
 module "azurerm_api_management" {
   source              = "git@github.com:teamdigitale/terraform-azurerm-resource.git"
   api_version         = "2019-01-01"
@@ -40,7 +28,7 @@ module "azurerm_api_management" {
   name                = "${local.azurerm_apim_name}"
   resource_group_name = "${data.azurerm_resource_group.rg.name}"
   location            = "${var.location}"
-  enable_output       = false
+  enable_output       = true
 
   random_deployment_name = true
 
@@ -66,40 +54,15 @@ module "azurerm_api_management" {
   }
 }
 
-# resource "null_resource" "azurerm_apim" {
-#   count = "${var.create ? 1 : 0}"
+resource "azurerm_api_management_group" "group" {
+  count               = "${length(var.apim_groups)}"
+  name                = "${lookup(var.apim_groups[count.index],"name")}"
+  resource_group_name = "${data.azurerm_resource_group.rg.name}"
+  api_management_name = "${local.azurerm_apim_name}"
+  display_name        = "${lookup(var.apim_groups[count.index],"display_name","default")}"
+  description         = "${lookup(var.apim_groups[count.index],"description","---")}"
+  type                = "${lookup(var.apim_groups[count.index],"type","custom")}"
+}
 
-
-#   triggers = {
-#     azurerm_function_app_name = "${local.azurerm_function_app_name}"
-
-
-#     provisioner_version = "${var.provisioner_version}"
-#   }
-
-
-#   provisioner "local-exec" {
-#     command = "${join(" ", list(
-#       "ts-node --log-error --files ${var.apim_provisioner}",
-#       "--environment ${var.environment}",
-#       "--azurerm_resource_group ${data.azurerm_resource_group.rg.name}",
-#       "--azurerm_apim ${local.azurerm_apim_name}",
-#       "--azurerm_apim_scm_url https://${local.azurerm_apim_name}.scm.azure-api.net/",
-#       "--azurerm_functionapp ${data.azurerm_app_service.function.name}",
-#       "--apim_configuration_path ${var.apim_configuration_path}"))
-#     }"
-
-
-#     environment = {
-#       ENVIRONMENT = "${var.environment}"
-
-
-#       TF_VAR_ADB2C_TENANT_ID = "${var.ADB2C_TENANT_ID}"
-
-
-#       TF_VAR_DEV_PORTAL_CLIENT_ID     = "${data.azurerm_key_vault_secret.dev_portal_client_id.value}"
-#       TF_VAR_DEV_PORTAL_CLIENT_SECRET = "${data.azurerm_key_vault_secret.dev_portal_client_secret.value}"
-#     }
-#   }
-# }
+# "${lookup(var.apim_groups[count.index],"name")}"
 
--- azurerm_api_management/variables.tf
+++ azurerm_api_management/variables.tf
@@ -25,25 +25,13 @@ variable "apim_name" {
 
 locals {
   # Define resource names based on the following convention:  # {azurerm_resource_name_prefix}-RESOURCE_TYPE-{environment}
-  azurerm_resource_group_name  = "${var.resource_name_prefix}-${var.environment}-rg"
-  azurerm_apim_name            = "${var.resource_name_prefix}-${var.environment}-apim-${var.apim_name}"
-  azurerm_function_app_name    = "${var.resource_name_prefix}-${var.environment}-fn-${var.function_app_name}"
-  azurerm_apim_scmurl          = "https://${var.resource_name_prefix}-apim-${var.environment}.scm.azure-api.net/"
+  azurerm_resource_group_name = "${var.resource_name_prefix}-${var.environment}-rg"
+  azurerm_apim_name           = "${var.resource_name_prefix}-${var.environment}-apim-${var.apim_name}"
+  azurerm_function_app_name   = "${var.resource_name_prefix}-${var.environment}-fn-${var.function_app_name}"
+
+  # azurerm_apim_scmurl          = "https://${var.resource_name_prefix}-apim-${var.environment}.scm.azure-api.net/"
   azurerm_virtual_network_name = "${var.resource_name_prefix}-${var.environment}-vnet-${var.vnet_name}"
   azurerm_subnet_name          = "${var.resource_name_prefix}-${var.environment}-subnet-${var.subnet_name}"
-
-  # locals {
-  #   # Common tags to be assigned to all resources
-  # "${merge(map("Name", (var.single_nat_gateway ? "${var.name}-${var.private_subnet_suffix}" : format("%s-${var.private_subnet_suffix}-%s", var.name, element(var.azs, count.index)))), var.tags, var.private_route_table_tags)}"
-  #   apim_properties = {
-  #     publisherEmail              = "${var.publisher_email}"
-  #     publisherName               = "${var.publisher_name}"
-  #     virtualNetworkType          = "${var.virtualNetworkType}"
-  #     # TODO: need a conditional to remove name when value is null
-  #     # virtualNetworkConfiguration = "${var.virtualNetworkConfiguration}"
-  #     # customProperties            = "${var.customProperties}"
-  #   }
-  # }
 }
 
 # TF_VAR_ADB2C_TENANT_ID
@@ -51,12 +39,6 @@ variable "ADB2C_TENANT_ID" {
   type        = "string"
   description = "Name of the Active Directory B2C tenant used in the API management portal authentication flow"
   default     = "agidweb"
-}
-
-# TODO: needs refactoring
-variable "apim_configuration_path" {
-  default     = "apim.json"
-  description = "Path of the (json) file that contains the configuration settings for the API management resource"
 }
 
 variable "notification_sender_email" {
@@ -85,10 +67,6 @@ variable "sku_capacity" {
   default     = 1
 }
 
-variable "key_vault_id" {
-  description = "Key Vauld id"
-}
-
 variable "azurerm_function_app_name" {
   description = "Function app name"
   default     = "undefined"
@@ -98,12 +76,6 @@ variable "virtualNetworkType" {
   description = "The type of VPN in which API Management service needs to be configured in. None (Default Value) means the API Management service is not part of any Virtual Network, External means the API Management deployment is set up inside a Virtual Network having an Internet Facing Endpoint, and Internal means that API Management deployment is setup inside a Virtual Network having an Intranet Facing Endpoint only."
   default     = "external"
 }
-
-# variable "subnetResourceId" {
-#   description = "The full resource ID of a subnet in a virtual network to deploy the API Management service in."
-#   type        = "string"
-#   default     = ""
-# }
 
 variable "tags" {
   description = "A map of tags to add to all resources"
@@ -126,15 +98,6 @@ variable "customProperties" {
   default = {}
 }
 
-variable "provisioner_version" {
-  description = "Force re-provisioning of API Management Git configuration"
-  default     = "1"
-}
-
-variable "apim_provisioner" {
-  default = "local-provisioners/azurerm_apim.ts"
-}
-
 variable "eventhub_name" {
   default = ""
 }
@@ -147,4 +110,9 @@ variable "vnet_name" {
 variable "subnet_name" {
   description = "The subnet name used by function_app"
   default     = ""
+}
+
+variable "apim_groups" {
+  description = "Api Management groups."
+  default     = []
 }
