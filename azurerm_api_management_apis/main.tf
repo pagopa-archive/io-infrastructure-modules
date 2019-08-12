7149f885f6cd8906d468415f8f630027255dd2ba
Luca Pinna <>
2019-08-12 12:22:51 +0200

    add api policy

--- azurerm_api_management_apis/main.tf
+++ azurerm_api_management_apis/main.tf
@@ -35,7 +35,7 @@ resource "azurerm_api_management_api" "apim_apis" {
 
 data "template_file" "api_defs" {
   count    = "${length(var.apim_apis)}"
-  template = "${file("${lookup(var.apim_apis[count.index],"name")}.json")}"
+  template = "${file("${lookup(var.apim_apis[count.index],"name")}/swagger.json")}"
 
   vars = {
     display_name = "${lookup(var.apim_apis[count.index],"display_name","${lookup(var.apim_apis[count.index],"name")}")}"
@@ -43,6 +43,15 @@ data "template_file" "api_defs" {
     host         = "${lookup(var.apim_apis[count.index],"host","api.example.com")}"
     path         = "${lookup(var.apim_apis[count.index],"path","")}"
   }
+}
+
+resource "azurerm_api_management_api_policy" "apim_api_policies" {
+  count               = "${length(var.apim_apis)}"
+  api_name            = "${lookup(var.apim_apis[count.index],"name")}"
+  api_management_name = "${basename(data.azurerm_api_management.api_management.id)}"
+  resource_group_name = "${data.azurerm_resource_group.rg.name}"
+
+  xml_content = "${file("${lookup(var.apim_apis[count.index],"name")}/${lookup(var.apim_apis[count.index],"xml_filename","default.xml")}")}"
 }
 
 resource "azurerm_api_management_api_operation" "apim_api_operations" {
@@ -55,40 +64,7 @@ resource "azurerm_api_management_api_operation" "apim_api_operations" {
   method              = "${lookup(var.apim_api_operations[count.index],"method","GET")}"
   url_template        = "${lookup(var.apim_api_operations[count.index],"url_template")}"
   description         = "${lookup(var.apim_api_operations[count.index],"description","---")}"
-
-  # template_parameter = ["${element(data.external.apim_api_operations_template_parameter.*.result,count.index)}"]
-  # template_parameter = ["${lookup(element(data.external.apim_api_operations_template_parameter.*.result,count.index),"name","") != ""? list(element(data.external.apim_api_operations_template_parameter.*.result,count.index)) : "" }"]
-  ## template_parameter = ["${element(data.external.apim_api_operations_template_parameter.*.result,count.index)}"]
-
-  # "templateParameters": [
-  #                   {
-  #                       "name": "serviceId",
-  #                       "required": true,
-  #                       "values": [],
-  #                       "type": "[parameters('operations_5a05960ad812e699f31de771_type')]"
-  #                   }
-  #               ]
 }
-
-# output "test" {
-#   value = "${data.null_data_source.apim_api_operations_template_parameter.*.outputs["template_parameter"]}"
-# }
-
-# data "null_data_source" "apim_api_operations_template_parameter" {
-#   count = "${length(var.apim_api_operations)}"
-
-#   inputs = {
-#     template_parameter = "${lookup(var.apim_api_operations[count.index],"template_parameter","[]")}"
-#   }
-# }
-# data "external" "apim_api_operations_template_parameter" {
-#   count   = "${length(var.apim_api_operations)}"
-#   program = ["echo", "${lookup(var.apim_api_operations[count.index],"template_parameter","{}")}"]
-# }
-
-# output "value1" {
-#   value = "${data.external.apim_api_operations_template_parameter.*.result}"
-# }
 
 resource "azurerm_api_management_api_operation_policy" "apim_api_operation_policies" {
   count               = "${length(var.apim_api_operation_policies)}"
@@ -96,7 +72,5 @@ resource "azurerm_api_management_api_operation_policy" "apim_api_operation_polic
   api_management_name = "${basename(data.azurerm_api_management.api_management.id)}"
   resource_group_name = "${data.azurerm_resource_group.rg.name}"
   operation_id        = "${lookup(var.apim_api_operation_policies[count.index],"operation_id")}"
-
-  # xml_content         = "${lookup(var.apim_api_operation_policies[count.index],"xml_filename","${local.api_management_default_api_operation_policy}")}"
-  xml_content = "${file("${lookup(var.apim_api_operation_policies[count.index],"xml_filename")}")}"
+  xml_content         = "${file("${lookup(var.apim_api_operation_policies[count.index],"api_name")}/${lookup(var.apim_api_operation_policies[count.index],"xml_filename","${lookup(var.apim_api_operation_policies[count.index],"operation_id")}.xml")}")}"
 }
