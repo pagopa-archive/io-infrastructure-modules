4ce2cd6d61e261ff10a24c2dbee4c3e923e0f3f7
Roberto Chessa <rchessa@users.noreply.github.com>
2019-08-28 17:40:37 +0200

    [#168170967] Cleanup TF function_app config module

---
+++ azurerm_function_app_config_clean/output.tf
@@ -0,0 +1,4 @@
+output "name" {
+  description = "The function app name"
+  value       = "${local.azurerm_functionapp_name}"
+}
---
+++ azurerm_function_app_config_clean/variables.tf
@@ -0,0 +1,37 @@
+variable "environment" {
+  description = "The nick name identifying the type of environment (i.e. test, staging, production)"
+}
+
+variable "location" {
+  description = "The data center location where all resources will be put into."
+}
+
+variable "resource_name_prefix" {
+  description = "The prefix used to name all resources created."
+}
+
+variable "vnet_name" {
+  description = "The vnet name used by function_app"
+  default     = ""
+}
+
+variable "subnet_name" {
+  description = "The subnet name used by function_app"
+  default     = ""
+}
+
+variable "functionapp_name" {
+  description = "The name suffix of the functionapp."
+}
+variable "azurerm_functionapp_reservedInstanceCount" {
+  description = "Number of reserved instances (to prevent cold start).This setting only applies to the Consumption and Premium Plan"
+  default     = "1"
+}
+
+locals {
+  # Define resource names based on the following convention:  # {azurerm_resource_name_prefix}-RESOURCE_TYPE-{environment}
+  azurerm_resource_group_name  = "${var.resource_name_prefix}-${var.environment}-rg"
+  azurerm_functionapp_name     = "${var.resource_name_prefix}-${var.environment}-fn-${var.functionapp_name}"
+  azurerm_virtual_network_name = "${var.resource_name_prefix}-${var.environment}-vnet-${var.vnet_name}"
+  azurerm_subnet_name          = "${var.resource_name_prefix}-${var.environment}-subnet-${var.subnet_name}"
+}
