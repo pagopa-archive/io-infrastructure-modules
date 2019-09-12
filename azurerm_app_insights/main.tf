7d01fd418883c91251a85b7493a639a0fc66b6a7
Riccardo Setti <rsetti@gmail.com>
2019-09-12 11:55:45 +0200

    168452151 allow multiple application insights to be created

--- azurerm_app_insights/main.tf
+++ azurerm_app_insights/main.tf
@@ -7,8 +7,9 @@ data "azurerm_resource_group" "rg" {
 # New infrastructure
 
 resource "azurerm_application_insights" "azurerm_application_insights" {
-  name                = "${local.azurerm_application_insights_name}"
+  count               = "${length(var.applications_insights)}"
+  name                = "${var.resource_name_prefix}-${var.environment}-ai-${lookup(var.applications_insights[count.index], "azurerm_application_insights_name")}"
   location            = "${data.azurerm_resource_group.rg.location}"
   resource_group_name = "${data.azurerm_resource_group.rg.name}"
-  application_type    = "${var.azurerm_application_insights_application_type}"
+  application_type    = "${lookup(var.applications_insights[count.index], "azurerm_application_insights_application_type")}"
 }
--- azurerm_app_insights/variables.tf
+++ azurerm_app_insights/variables.tf
@@ -1,27 +1,21 @@
-# General Variables
+# Generic variables
 
 variable "environment" {
   description = "The nick name identifying the type of environment (i.e. test, staging, production)"
-}
-
-variable "location" {
-  description = "The data center location where all resources will be put into."
 }
 
 variable "resource_name_prefix" {
   description = "The prefix used to name all resources created."
 }
 
-# App service plan specific variables
-
-variable "azurerm_application_insights_name" {
-  description = "The value that will be used as suffix to identify the app insights resource."
-}
-variable "azurerm_application_insights_application_type" {
-  description = "the type of Application Insights."
+# Specific variables for application insights
+variable "applications_insights" {
+  type        = "list"
+  default     = []
+  description = "A list of map(s) that contains one or more alert definition. Please read README.md to the list of keys required to be present"
 }
 
 locals {
-    azurerm_resource_group_name   = "${var.resource_name_prefix}-${var.environment}-rg"
-    azurerm_application_insights_name     = "${var.resource_name_prefix}-${var.environment}-ai-${var.azurerm_application_insights_name}"
+  # Define resource names based on the following convention:  # {azurerm_resource_name_prefix}-{environment}-RESOURCE_TYPE-SUFFIX
+  azurerm_resource_group_name   = "${var.resource_name_prefix}-${var.environment}-rg"
 }
