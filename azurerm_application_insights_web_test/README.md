ae274a7bc7d41f8c7d6bf67979c42b90127dc0c7
Riccardo Setti <giskard@autistici.org>
2019-09-30 14:23:01 +0200

    [168800206] Add support for headers in XML requests (#33)

---
+++ azurerm_application_insights_web_test/README.md
@@ -0,0 +1,34 @@
+# azurerm\_application\_insight\_web\_test basic usage:
+
+## Web test terraform representation:
+
+A web test is an entry in the `web_tests` variable, defined as a map of keys, like the one below:
+
+```
+web_tests = [
+  {
+  	name        = <string>
+  	url         = <string>
+  	headers_xml = <string>
+   },
+  ...
+]
+```
+
+## Alerts composition:
+
+Alert definitions require the *name*, *url* and *headers_xml* variables:
+
+```
+ variable "name" {
+   description = "The name of the web test.
+ }
+
+ variable "url" {
+   description = "the url we want to monitor"
+ }
+	
+ variable "headers_xml" {
+   description = "A string(s) that contains one or more <Header Key="" Value=" />"
+ }
+```
--- azurerm_application_insights_web_test/main.tf
+++ azurerm_application_insights_web_test/main.tf
@@ -22,6 +22,7 @@ resource "azurerm_application_insights_web_test" "web_test" {
   kind          = "ping"
   retry_enabled = true
   timeout       = 30
+  enabled       = true
 
   location      = "${data.azurerm_resource_group.rg.location}"
   geo_locations = [
@@ -30,10 +31,21 @@ resource "azurerm_application_insights_web_test" "web_test" {
   ]
 
   configuration = <<XML
-<WebTest Name="${lookup(var.web_tests[count.index], "name")}" Id="ABD48585-0831-40CB-9069-682EA6BB3583" Enabled="True" CssProjectStructure="" CssIteration="" Timeout="0" WorkItemIds="" xmlns="http://microsoft.com/schemas/VisualStudio/TeamTest/2010" Description="" CredentialUserName="" CredentialPassword="" PreAuthenticate="True" Proxy="default" StopOnError="False" RecordedResultFile="" ResultsLocale="">
-  <Items>
-    <Request Method="GET" Guid="a5f10126-e4cd-570d-961c-cea43999a200" Version="1.1" Url="${lookup(var.web_tests[count.index], "url")}" ThinkTime="0" Timeout="300" ParseDependentRequests="True" FollowRedirects="True" RecordResult="True" Cache="False" ResponseTimeGoal="0" Encoding="utf-8" ExpectedHttpStatusCode="200" ExpectedResponseUrl="" ReportingName="" IgnoreHttpStatusCode="False" />
-  </Items>
-</WebTest>
+  <WebTest Name="${lookup(var.web_tests[count.index], "name")}" Id="ABD48585-0831-40CB-9069-682EA6BB3583" Enabled="True"
+	   CssProjectStructure="" CssIteration="" Timeout="0" WorkItemIds=""
+	   xmlns="http://microsoft.com/schemas/VisualStudio/TeamTest/2010"
+	   Description="" CredentialUserName="" CredentialPassword="" PreAuthenticate="True" Proxy="default"
+	   StopOnError="False" RecordedResultFile="" ResultsLocale="">
+    <Items>
+      <Request Method="GET" Guid="a5f10126-e4cd-570d-961c-cea43999a200" Version="1.1" Url="${lookup(var.web_tests[count.index], "url")}"
+	ThinkTime="0" Timeout="300" ParseDependentRequests="True" FollowRedirects="True" RecordResult="True"
+	Cache="False" ResponseTimeGoal="0" Encoding="utf-8" ExpectedHttpStatusCode="200"
+	ExpectedResponseUrl="" ReportingName="" IgnoreHttpStatusCode="False">
+	<Headers>
+          ${lookup(var.web_tests[count.index], "headers_xml", "")}
+	</Headers>
+      </Request>
+    </Items>
+  </WebTest>
 XML
 }
