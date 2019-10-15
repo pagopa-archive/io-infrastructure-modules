1449c66cb9227a4d09c5bfdfb8db3aab65aa026d
Riccardo Setti <giskard@autistici.org>
2019-10-15 09:43:35 +0200

    [#169145231] web-test: split out xml generation (#39)

--- azurerm_application_insights_web_test/main.tf
+++ azurerm_application_insights_web_test/main.tf
@@ -31,23 +31,6 @@ resource "azurerm_application_insights_web_test" "web_test" {
   ]
 
   configuration = <<XML
-  <WebTest Name="${lookup(var.web_tests[count.index], "name")}" Id="ABD48585-0831-40CB-9069-682EA6BB3583" Enabled="True"
-	   CssProjectStructure="" CssIteration="" Timeout="0" WorkItemIds=""
-	   xmlns="http://microsoft.com/schemas/VisualStudio/TeamTest/2010"
-	   Description="" CredentialUserName="" CredentialPassword="" PreAuthenticate="True" Proxy="default"
-	   StopOnError="False" RecordedResultFile="" ResultsLocale="">
-    <Items>
-      <Request Method="${lookup(var.web_tests[count.index], "method", "GET")}" Guid="a5f10126-e4cd-570d-961c-cea43999a200"
-        Version="1.1" Url="${lookup(var.web_tests[count.index], "url")}"
-	ThinkTime="0" Timeout="300" ParseDependentRequests="True" FollowRedirects="True" RecordResult="True"
-	Cache="False" ResponseTimeGoal="0" Encoding="utf-8" ExpectedHttpStatusCode="200"
-	ExpectedResponseUrl="" ReportingName="" IgnoreHttpStatusCode="False">
-	<Headers>
-          ${lookup(var.web_tests[count.index], "headers_xml", "")}
-	</Headers>
-	<StringHttpBody ContentType="application/json">${lookup(var.web_tests[count.index], "body", "")}</StringHttpBody>
-      </Request>
-    </Items>
-  </WebTest>
+${lookup(var.web_tests[count.index], "xml")}"
 XML
 }
