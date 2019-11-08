e83c463e8a678005ae44e6882ae75eabfa77b1df
Roberto Chessa <rchessa@users.noreply.github.com>
2019-11-08 11:50:05 +0100

    [#169643706] Add subnet TF module for k8s (#43)

---
+++ azurerm_subnet_k8s/main.tf
@@ -0,0 +1,23 @@
+# Existing infrastructure
+
+data "azurerm_resource_group" "rg" {
+  name = "${local.azurerm_resource_group_name}"
+}
+
+data "azurerm_virtual_network" "vnet" {
+  name                = "${local.azurerm_virtual_network_name}"
+  resource_group_name = "${data.azurerm_resource_group.rg.name}"
+}
+
+# New infrastructure
+resource "azurerm_subnet" "subnet" {
+  name                      = "${local.azurerm_subnet_name}"
+  resource_group_name       = "${data.azurerm_resource_group.rg.name}"
+  address_prefix            = "${var.azurerm_subnet_address_prefix}"
+  virtual_network_name      = "${data.azurerm_virtual_network.vnet.name}"
+  service_endpoints         = "${var.azurerm_subnet_service_endpoints}"
+
+  lifecycle {
+    ignore_changes = ["route_table_id"]
+  }
+}
