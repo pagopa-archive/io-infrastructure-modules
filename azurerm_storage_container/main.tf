d693cb92d2448cd16bf9fe2922391e8efb2be094
Luca Prete <preteluca@gmail.com>
2019-11-11 15:12:52 +0100

    [#169679636] Remove deprecated resource_group from azurerm_storage_container TF module

--- azurerm_storage_container/main.tf
+++ azurerm_storage_container/main.tf
@@ -9,7 +9,6 @@ data "azurerm_storage_account" "sa" {
 
 resource "azurerm_storage_container" "container" {
   name                  = "${var.azurerm_storage_container_name}"
-  resource_group_name   = "${data.azurerm_resource_group.rg.name}"
   storage_account_name  = "${data.azurerm_storage_account.sa.name}"
   container_access_type = "${var.azurerm_storage_container_access_type}"
 }
