e26f425f7151c7576ce561ad2d7831e2ebee86d5
Roberto Chessa <rchessa@users.noreply.github.com>
2019-11-22 10:27:08 +0100

    [#169913130] Update AppInsights to store instrumentation_key on keyvault (#47)

--- azurerm_app_insights/main.tf
+++ azurerm_app_insights/main.tf
@@ -4,6 +4,11 @@ data "azurerm_resource_group" "rg" {
   name = "${local.azurerm_resource_group_name}"
 }
 
+data "azurerm_key_vault" "key_vault" {
+  name                = "${local.azurerm_key_vault_name}"
+  resource_group_name = "${data.azurerm_resource_group.rg.name}"
+}
+
 # New infrastructure
 
 resource "azurerm_application_insights" "azurerm_application_insights" {
@@ -13,3 +18,10 @@ resource "azurerm_application_insights" "azurerm_application_insights" {
   resource_group_name = "${data.azurerm_resource_group.rg.name}"
   application_type    = "${lookup(var.applications_insights[count.index], "azurerm_application_insights_application_type")}"
 }
+
+resource "azurerm_key_vault_secret" "azurerm_application_insights" {
+  name         = "${var.azurerm_application_insights_instrumentation_key}"
+  value        = "${azurerm_application_insights.azurerm_application_insights.instrumentation_key}"
+  key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
+}
+
