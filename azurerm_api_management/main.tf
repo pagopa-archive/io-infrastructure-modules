6cd1fe9b1cc0c42e1aa8f0c5932715e91bbf1bf5
Roberto Chessa <rchessa@users.noreply.github.com>
2019-11-29 16:45:59 +0100

    [#169998800] Refactoring APIM module to support custom domains (#49)

--- azurerm_api_management/main.tf
+++ azurerm_api_management/main.tf
@@ -8,6 +8,11 @@ data "azurerm_resource_group" "rg" {
   name = "${local.azurerm_resource_group_name}"
 }
 
+data "azurerm_key_vault" "key_vault" {
+  name                = "${local.azurerm_key_vault_name}"
+  resource_group_name = "${data.azurerm_resource_group.rg.name}"
+}
+
 data "azurerm_virtual_network" "vnet" {
   name                = "${local.azurerm_virtual_network_name}"
   resource_group_name = "${data.azurerm_resource_group.rg.name}"
@@ -19,59 +24,25 @@ data "azurerm_subnet" "apim_subnet" {
   resource_group_name  = "${data.azurerm_resource_group.rg.name}"
 }
 
-data "null_data_source" "hostname_configurations" {
-  count  = "${length(var.hostname_configurations)}"
-
-  inputs = {
-    type                       = "Proxy"
-    hostName                   = "${lookup(var.hostname_configurations[count.index],"hostname_prefix")}.${var.environment}.io.italia.it"
-    negotiateClientCertificate = "false"
-    defaultSslBinding          = "true"
-    keyVaultId                 = "https://${local.azurerm_key_vault_name}.vault.azure.net/secrets/generated-cert"
-  }
-}
+data "azurerm_client_config" "current" {}
 
 # New infrastructure
 
-module "azurerm_api_management" {
-  source                 = "git@github.com:teamdigitale/terraform-azurerm-resource.git"
-  api_version            = "2019-01-01"
-  type                   = "Microsoft.ApiManagement/service"
-  name                   = "${local.azurerm_apim_name}"
-  resource_group_name    = "${data.azurerm_resource_group.rg.name}"
-  location               = "${var.location}"
-  enable_output          = true
-
-  random_deployment_name = true
-
-  properties {
-    publisherEmail              = "${var.publisher_email}"
-    publisherName               = "${var.publisher_name}"
-    virtualNetworkType          = "${var.virtualNetworkType}"
-
-    virtualNetworkConfiguration = {
-      subnetResourceId = "${data.azurerm_subnet.apim_subnet.id}"
-    }
-
-    hostnameConfigurations      = [
-      {
-        type                       = "Proxy"
-        hostName                   = "${local.hostname_configurations_hostname}"
-        negotiateClientCertificate = "false"
-        defaultSslBinding          = "true"
-        keyVaultId                 = "${local.hostname_configurations_keyvault_id}"
-      }
-    ]
-
-    customProperties            = "${var.customProperties}"
+resource "azurerm_template_deployment" "apim" {
+  # name is the deploymeny name on azure
+  name                = "apim"
+  resource_group_name = "${data.azurerm_resource_group.rg.name}"
+  template_body       = "${file("apim-create.json")}"
+  parameters          = {
+    "publisherEmail"          = "${var.publisher_email}"
+    "publisherName"           = "${var.publisher_name}"
+    "sku"                     = "${var.sku_name}"
+    "proxyCustomHostname1"    = "${local.hostname_configurations_hostname}"
+    "keyVaultIdToCertificate" = "${local.hostname_configurations_keyvault_id}"
+    "keyVaultName"            = "${local.azurerm_key_vault_name}"
+    "apiName"                 = "${local.azurerm_apim_name}"
+    "location"                = "${data.azurerm_resource_group.rg.location}"
+    "subnetRef"               = "${data.azurerm_subnet.apim_subnet.id}"
   }
-
-  sku {
-    name     = "${var.sku_name}"
-    capacity = "${var.sku_capacity}"
-  }
-
-  tags = {
-    environment = "${var.environment}"
-  }
+  deployment_mode = "Incremental"
 }
