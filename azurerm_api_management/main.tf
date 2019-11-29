772cb34b5a6a18e30eb84abcd369d48fce3fa81e
Luca Prete <lucaprete@teamdigitale.governo.it>
2019-11-29 16:37:46 +0100

    Update main.tf

--- azurerm_api_management/main.tf
+++ azurerm_api_management/main.tf
@@ -29,23 +29,20 @@ data "azurerm_client_config" "current" {}
 # New infrastructure
 
 resource "azurerm_template_deployment" "apim" {
+  # name is the deploymeny name on azure
   name                = "apim"
   resource_group_name = "${data.azurerm_resource_group.rg.name}"
-
-  template_body = "${file("apim-create.json")}"
-
-  parameters = {
-    "publisherEmail"           = "${var.publisher_email}"
-    "publisherName"            = "${var.publisher_name}"
-    "sku"                      = "${var.sku_name}"
-    "proxyCustomHostname1"     = "${local.hostname_configurations_hostname}"
-    "keyVaultIdToCertificate"  = "${local.hostname_configurations_keyvault_id}"
-    "keyVaultName"             = "${local.azurerm_key_vault_name}"
-    "apiName"                  = "${local.azurerm_apim_name}"
-    "location"                 = "${data.azurerm_resource_group.rg.location}"
-	"subnetRef"                = "${data.azurerm_subnet.apim_subnet.id}"
+  template_body       = "${file("apim-create.json")}"
+  parameters          = {
+    "publisherEmail"          = "${var.publisher_email}"
+    "publisherName"           = "${var.publisher_name}"
+    "sku"                     = "${var.sku_name}"
+    "proxyCustomHostname1"    = "${local.hostname_configurations_hostname}"
+    "keyVaultIdToCertificate" = "${local.hostname_configurations_keyvault_id}"
+    "keyVaultName"            = "${local.azurerm_key_vault_name}"
+    "apiName"                 = "${local.azurerm_apim_name}"
+    "location"                = "${data.azurerm_resource_group.rg.location}"
+    "subnetRef"               = "${data.azurerm_subnet.apim_subnet.id}"
   }
-
   deployment_mode = "Incremental"
-
 }
