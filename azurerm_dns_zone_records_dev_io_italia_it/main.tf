09454210f5e8addb7903a1863c49329561895ac1
Luca Prete <preteluca@gmail.com>
2019-12-03 12:30:52 +0100

    [#170073446] Remove k8s public IPs and modify DNSs to connect to application gateway k8s IPs

--- azurerm_dns_zone_records_dev_io_italia_it/main.tf
+++ azurerm_dns_zone_records_dev_io_italia_it/main.tf
@@ -4,10 +4,6 @@ data "azurerm_resource_group" "rg" {
   name = "${local.azurerm_resource_group_name}"
 }
 
-data "azurerm_resource_group" "aks_rg" {
-  name = "${var.kubernetes_resource_group_name}"
-}
-
 data "azurerm_dns_zone" "dns_zone" {
   name                = "${local.azurerm_dns_zone_name}"
   resource_group_name = "${data.azurerm_resource_group.rg.name}"
@@ -15,7 +11,7 @@ data "azurerm_dns_zone" "dns_zone" {
 
 data "azurerm_public_ip" "kubernetes_public_ip" {
   name                = "${local.kubernetes_public_ip_name}"
-  resource_group_name = "${data.azurerm_resource_group.aks_rg.name}"
+  resource_group_name = "${data.azurerm_resource_group.rg.name}"
 }
 
 # New infrastructure
