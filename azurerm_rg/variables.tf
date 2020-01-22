30b02792662994dfbc90966276cd1230485736ce
Roberto Chessa <r.chessa@beetobit.com>
2020-01-22 16:11:42 +0100

    #170823974 add suffix for rg

--- azurerm_rg/variables.tf
+++ azurerm_rg/variables.tf
@@ -10,8 +10,14 @@ variable "resource_name_prefix" {
   description = "The prefix used to name all resources created."
 }
 
+variable "resource_name_suffix" {
+  description = "The suffix used to name all resources created."
+  default     = ""
+}
+
 locals {
   # Define resource names based on the following convention:
   # {azurerm_resource_name_prefix}-RESOURCE_TYPE-{environment}
-  azurerm_resource_group_name = "${var.resource_name_prefix}-${var.environment}-rg"
+  #azurerm_resource_group_name = "${var.resource_name_prefix}-${var.environment}-rg"
+  azurerm_resource_group_name = "${var.resource_name_prefix}-${var.environment}${var.resource_name_suffix != "" ? "-${var.resource_name_suffix}-" : "-"}rg"
 }
