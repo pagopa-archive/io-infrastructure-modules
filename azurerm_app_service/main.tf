747c08039a106a5fd63c8fafb2b09e03d1565aae
Roberto Chessa <r.chessa@beetobit.com>
2020-01-27 10:46:22 +0100

    #170824084 add app_service module

---
+++ azurerm_app_service/main.tf
@@ -0,0 +1,202 @@
+data "azurerm_resource_group" "rg" {
+    name = "${local.azurerm_resource_group_name}"
+}
+data "azurerm_app_service_plan" "plan" {
+    name                = "${local.azurerm_service_plan_name}"
+    resource_group_name = "${data.azurerm_resource_group.rg.name}"
+}
+data "azurerm_log_analytics_workspace" "log_analytics_workspace" {
+    name                = "${local.azurerm_log_analytics_workspace_name}"
+    resource_group_name = "${local.azurerm_resource_group_name_main}"
+}
+data "azurerm_monitor_diagnostic_categories" "diag" {
+    resource_id = "${azurerm_app_service.app_service.id}"
+}
+data "azurerm_virtual_network" "vnet" {
+    count               = "${var.ip_restriction}"
+    name                = "${local.azurerm_virtual_network_name}"
+    resource_group_name = "${local.azurerm_resource_group_name_main}"
+}
+data "azurerm_subnet" "subnet" {
+    count                = "${var.ip_restriction}"  
+    name                 = "${local.azurerm_subnet_name}"
+    virtual_network_name = "${data.azurerm_virtual_network.vnet.name}"
+    resource_group_name  = "${local.azurerm_resource_group_name_main}"
+}
+resource "azurerm_app_service" "app_service" {
+    count               = "${1 - var.ip_restriction}"
+    name                = "${local.azurerm_app_name}"
+    resource_group_name = "${data.azurerm_resource_group.rg.name}"
+    location            = "${data.azurerm_resource_group.rg.location}"
+    app_service_plan_id = "${data.azurerm_app_service_plan.plan.id}"
+
+    site_config {
+        linux_fx_version = "${var.docker_image}"
+        app_command_line = ""
+    }
+
+    app_settings {
+        DOCKER_REGISTRY_SERVER_USERNAME     = "${var.docker_registry_server_username}"
+        DOCKER_REGISTRY_SERVER_URL          = "${var.docker_registry_server_url}"
+        DOCKER_REGISTRY_SERVER_PASSWORD     = "${var.docker_registry_server_password}"
+        WEBSITES_ENABLE_APP_SERVICE_STORAGE = "${var.websites_enable_app_service_storage}" 
+    }
+
+    lifecycle {
+        ignore_changes = [
+        "site_config.0.linux_fx_version", # deployments are made outside of Terraform
+        ]
+    } 
+}
+
+resource "azurerm_app_service" "app_service_restriction" {
+    count               = "${var.ip_restriction}"
+    name                = "${local.azurerm_app_name}"
+    resource_group_name = "${data.azurerm_resource_group.rg.name}"
+    location            = "${data.azurerm_resource_group.rg.location}"
+    app_service_plan_id = "${data.azurerm_app_service_plan.plan.id}"
+
+    site_config {
+        linux_fx_version = "${var.docker_image}"
+        app_command_line = ""
+
+        ip_restriction {
+            virtual_network_subnet_id = "${data.azurerm_subnet.subnet.id}"
+        }
+    }
+
+    app_settings {
+        DOCKER_REGISTRY_SERVER_USERNAME     = "${var.docker_registry_server_username}"
+        DOCKER_REGISTRY_SERVER_URL          = "${var.docker_registry_server_url}"
+        DOCKER_REGISTRY_SERVER_PASSWORD     = "${var.docker_registry_server_password}"
+        WEBSITES_ENABLE_APP_SERVICE_STORAGE = "${var.websites_enable_app_service_storage}" 
+    }
+
+    lifecycle {
+        ignore_changes = [
+        "site_config.0.linux_fx_version", # deployments are made outside of Terraform
+        ]
+    } 
+}
+// Add Azure Monitor Diagnostic
+resource "azurerm_monitor_diagnostic_setting" "diag" {
+  count                      = "${1 - var.ip_restriction}"
+  name                       = "${local.azurerm_app_service_diagnostic_name}"
+  target_resource_id         = "${azurerm_app_service.app_service.id}"
+  log_analytics_workspace_id = "${data.azurerm_log_analytics_workspace.log_analytics_workspace.id}"
+
+  log {
+    category = "${data.azurerm_monitor_diagnostic_categories.diag.logs[0]}"
+    enabled  = true
+
+    retention_policy {
+      enabled = true
+      days    = "${var.app_service_diagnostic_logs_retention}"
+    }
+  }
+  log {
+    category = "${data.azurerm_monitor_diagnostic_categories.diag.logs[1]}"
+    enabled  = true
+
+    retention_policy {
+      enabled = true
+      days    = "${var.app_service_diagnostic_logs_retention}"
+    }
+  }
+  log {
+    category = "${data.azurerm_monitor_diagnostic_categories.diag.logs[2]}"
+    enabled  = true
+
+    retention_policy {
+      enabled = true
+      days    = "${var.app_service_diagnostic_logs_retention}"
+    }
+  }
+  log {
+    category = "${data.azurerm_monitor_diagnostic_categories.diag.logs[3]}"
+    enabled  = true
+
+    retention_policy {
+      enabled = true
+      days    = "${var.app_service_diagnostic_logs_retention}"
+    }
+  }
+  log {
+    category = "${data.azurerm_monitor_diagnostic_categories.diag.logs[4]}"
+    enabled  = true
+
+    retention_policy {
+      enabled = true
+      days    = "${var.app_service_diagnostic_logs_retention}"
+    }
+  }
+  metric {
+    category = "${data.azurerm_monitor_diagnostic_categories.diag.metrics[0]}"
+
+    retention_policy {
+      enabled = true
+      days    = "${var.app_service_diagnostic_logs_retention}"
+    }
+  }
+}
+
+resource "azurerm_monitor_diagnostic_setting" "diag_restriction" {
+  count                      = "${var.ip_restriction}"
+  name                       = "${local.azurerm_app_service_diagnostic_name}"
+  target_resource_id         = "${azurerm_app_service.app_service_restriction.id}"
+  log_analytics_workspace_id = "${data.azurerm_log_analytics_workspace.log_analytics_workspace.id}"
+
+  log {
+    category = "${data.azurerm_monitor_diagnostic_categories.diag.logs[0]}"
+    enabled  = true
+
+    retention_policy {
+      enabled = true
+      days    = "${var.app_service_diagnostic_logs_retention}"
+    }
+  }
+  log {
+    category = "${data.azurerm_monitor_diagnostic_categories.diag.logs[1]}"
+    enabled  = true
+
+    retention_policy {
+      enabled = true
+      days    = "${var.app_service_diagnostic_logs_retention}"
+    }
+  }
+  log {
+    category = "${data.azurerm_monitor_diagnostic_categories.diag.logs[2]}"
+    enabled  = true
+
+    retention_policy {
+      enabled = true
+      days    = "${var.app_service_diagnostic_logs_retention}"
+    }
+  }
+  log {
+    category = "${data.azurerm_monitor_diagnostic_categories.diag.logs[3]}"
+    enabled  = true
+
+    retention_policy {
+      enabled = true
+      days    = "${var.app_service_diagnostic_logs_retention}"
+    }
+  }
+  log {
+    category = "${data.azurerm_monitor_diagnostic_categories.diag.logs[4]}"
+    enabled  = true
+
+    retention_policy {
+      enabled = true
+      days    = "${var.app_service_diagnostic_logs_retention}"
+    }
+  }
+  metric {
+    category = "${data.azurerm_monitor_diagnostic_categories.diag.metrics[0]}"
+
+    retention_policy {
+      enabled = true
+      days    = "${var.app_service_diagnostic_logs_retention}"
+    }
+  }
+}
---
+++ azurerm_app_service/variables.tf
@@ -0,0 +1,92 @@
+# General Variables
+
+variable "environment" {
+  description = "The nick name identifying the type of environment (i.e. test, staging, production)"
+}
+
+variable "location" {
+  description = "The data center location where all resources will be put into."
+}
+
+variable "resource_name_prefix" {
+  description = "The prefix used to name all resources created."
+}
+
+# App Service specific variables
+variable "docker_image" {
+  description = "The docker image and tag."
+}
+
+variable "docker_registry_server_username" {
+  description = "The docker registry username."
+  default     = ""
+}
+
+variable "docker_registry_server_url" {
+  description = "The docker registry url, default to docker-hub."
+  default     = "https://index.docker.io"
+}
+
+variable "docker_registry_server_password" {
+  description = "The docker registry password."
+  default     = ""
+}
+variable "websites_enable_app_service_storage" {
+  description = "If you need files to be persisted in scale operations or across restarts, you should add this app setting and set it to true. If you don't require file persistence, you can set this app setting to false."
+  default     = "false"
+}
+
+variable "virtual_network_subnet_id" {
+  description = "The subnet id used for access restriction"
+  default     = ""
+}
+variable "app_name_suffix" {
+  description = "description"
+  default     = ""
+}
+
+variable "service_plan_name_suffix" {
+  description = "description"
+  default     = ""
+}
+variable "resource_name_suffix" {
+  description = "description"
+  default     = ""
+}
+
+variable "app_service_diagnostic_logs_retention" {
+  description = "The retention days of logs."
+  default     = 30
+}
+variable "log_analytics_workspace_name" {
+  description = "The Log Analytics workspace name."
+  default     = "log-analytics-workspace"
+}
+variable "ip_restriction" {
+  description = "description"
+  default     = false
+}
+variable "subnet_id" {
+  description = "The subnet id."
+  default = ""
+}
+variable "vnet_name" {
+  description = "The name suffix of the virtual network where nodes and external load balancers' IPs will be created."
+}
+
+variable "subnet_name" {
+  description = "The name suffix of the subnet where nodes and external load balancers' IPs will be created."
+}
+
+locals {
+  # Define resource names based on the following convention:
+  # {resource_name_prefix}-{environment}-{resource_type}[-resource_name_suffix]
+  azurerm_resource_group_name          = "${var.resource_name_prefix}-${var.environment}${var.resource_name_suffix != "" ? "-${var.resource_name_suffix}-" : "-"}rg"
+  azurerm_resource_group_name_main     = "${var.resource_name_prefix}-${var.environment}-rg"
+  azurerm_service_plan_name            = "${var.resource_name_prefix}-${var.environment}-serviceplan${var.service_plan_name_suffix != "" ? "-${var.service_plan_name_suffix}" : ""}"
+  azurerm_app_name                     = "${var.resource_name_prefix}-${var.environment}-app${var.app_name_suffix != "" ? "-${var.app_name_suffix}" : ""}"
+  azurerm_log_analytics_workspace_name = "${var.resource_name_prefix}-${var.environment}-log-analytics-workspace-${var.log_analytics_workspace_name}"
+  azurerm_app_service_diagnostic_name  = "${var.resource_name_prefix}-${var.environment}-app-diagnostic-${var.app_name_suffix}"
+  azurerm_virtual_network_name         = "${var.resource_name_prefix}-${var.environment}-vnet-${var.vnet_name}"
+  azurerm_subnet_name                  = "${var.resource_name_prefix}-${var.environment}-subnet-${var.subnet_name}"
+}
