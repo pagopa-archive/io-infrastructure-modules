bfbcb4bfc743f5b0257ede4456adb0b8774d43ee
danilo.spinelli <danilo.spinelli@pagopa.it>
2020-02-13 13:48:50 +0100

    [#171254626] fix CDN modules

--- azurerm_cdn_custom_domain/variables.tf
+++ azurerm_cdn_custom_domain/variables.tf
@@ -14,18 +14,30 @@ variable "azurerm_cdn_endpoint_suffix" {
   description = "CDN endpoint resource suffix."
 }
 
-variable "azurerm_cdn_custom_domain_host_name" {
-  description = "CDN custom domain host name. Must match an existing CNAME alias (add the dns zone suffix)."
+variable "dns_zone_name" {
+  description = "The Azure DNS name suffix."
+}
+
+variable "dns_record_ttl" {
+  description = "The DNS records TTL in seconds."
+}
+
+variable "cname_record" {
+  description = "The CNAME DNS record value."
 }
 
 locals {
   # Define resource names based on the following convention:
   # {azurerm_resource_name_prefix}-{environment}-RESOURCE_TYPE-suffix
-  azurerm_resource_group_name    = "${var.resource_name_prefix}-${var.environment}-rg"
+  azurerm_resource_group_name                = "${var.resource_name_prefix}-${var.environment}-rg"
 
-  azurerm_cdn_profile_name       = "${var.resource_name_prefix}-${var.environment}-cdn-${var.azurerm_cdn_profile_suffix}"
+  azurerm_cdn_profile_name                   = "${var.resource_name_prefix}-${var.environment}-cdn-${var.azurerm_cdn_profile_suffix}"
 
-  azurerm_cdn_endpoint_name      = "${var.resource_name_prefix}-${var.environment}-cdn-endpoint-${var.azurerm_cdn_endpoint_suffix}"
+  azurerm_cdn_endpoint_name                  = "${var.resource_name_prefix}-${var.environment}-cdn-endpoint-${var.azurerm_cdn_endpoint_suffix}"
 
-  azurerm_cdn_custom_domain_name = "${replace(var.azurerm_cdn_custom_domain_host_name, ".", "-")}"
+  azurerm_cdn_endpoint_hostname              = "${var.resource_name_prefix}-${var.environment}-cdn-endpoint-${var.azurerm_cdn_endpoint_suffix}.azureedge.net"
+
+  azurerm_cdn_custom_domain_host_name        = "${var.cname_record}.${var.dns_zone_name}"
+
+  azurerm_cdn_custom_domain_name             = "${replace("${var.cname_record}.${var.dns_zone_name}", ".", "-")}"
 }
